106b617e2ed2cff0e2df4ac519a5f3fcd7c9b297
po3xmgroup <97295536+po3xmgroup@users.noreply.github.com>
2022-02-16 17:26:16 -0300

    Update s3.tf

--- modules/aws_s3/s3.tf
+++ modules/aws_s3/s3.tf
@@ -2,7 +2,21 @@ resource "aws_s3_bucket" "codepipeline_bucket" {
   bucket = var.s3_bucket_name
   acl    = "private"
   force_destroy = true
-  policy = templatefile("../../templates/${var.policy_path}", { aws_s3_bucket = "${var.s3_bucket_name}" })
+  policy = jsonencode({
+    "Id": "bucket_policy_site",
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "bucket_policy_site_main",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Effect": "Allow",
+        "Resource": "arn:aws:s3:::s3-static-website-training-dev/*",
+        "Principal": {"AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity E39OBE3O5KW9US"}
+      }
+    ]
+  })
 
   website {
     index_document = "index.html"
